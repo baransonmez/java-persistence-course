24e9a019dbacd6fcc08cca58aa8c554c690b6821
İhsan Baran SÖNMEZ <ibaranson@gmail.com>
2022-07-29 21:30:24 +0300

    update mapping value type test

--- 02-mapping-value-types/src/test/java/PersistenceTest.java
+++ 02-mapping-value-types/src/test/java/PersistenceTest.java
@@ -11,9 +11,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class PersistenceTest {
     private SessionFactory factory = null;
@@ -29,27 +27,20 @@ public class PersistenceTest {
                 .buildSessionFactory();
     }
 
-    public User saveUser(String userName, AccountType accountType, AccountStatus status, Address billingAddress, Address deliveryAddress) {
-        User user = new User(userName, accountType, status, billingAddress, deliveryAddress);
+    @Test
+    public void createUserTest() {
+        User user = new User("useame", AccountType.GOLD, AccountStatus.ACTIVE, new Address("emre cd", "06798", "Ankara"), new Address("emre cd", "06798", "Ankara"));
+
         try (Session session = factory.openSession()) {
             Transaction tx = session.beginTransaction();
             session.persist(user);
             tx.commit();
         }
-        return user;
-    }
-    @Test
-    public void readUser() {
-        saveUser("username", AccountType.GOLD, AccountStatus.ACTIVE, new Address("emre cd", "06798", "Ankara"), new Address("emre cd", "06798", "Ankara"));
-        List<User> list;
+
         try (Session session = factory.openSession()) {
-            list = session
-                    .createQuery("select u from users u", User.class)
-                    .list();
-        }
-        assertEquals(list.size(), 1);
-        for (User m : list) {
-            System.out.println(m);
+            User userFromDb = session.find(User.class, 1L);
+            assertNotNull(userFromDb);
+            System.out.println(userFromDb);
         }
     }
 }
